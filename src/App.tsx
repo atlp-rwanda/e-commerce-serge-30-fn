ba0caf832d7105556a03c0bc59e7dbe7b17d349a
martinemahirwe <martinemahirwe@gmail.com>
2024-06-12 15:32:23 +0200

    ch(store.ts): Setup redux toolkit for global state management Configured the Redux store using Redux Toolkit Created slices folder Provided the redux store into the react app Created custom hooks for useSelector and useDispatch with predefined types to avoid assigning types repeatedly [Delivers #187364794]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+
+import { Provider } from 'react-redux';
+import { store } from './store';
 import "./App.css";
 
 function App() {
   return (
-    <main className="bg-slate-900 h-screen flex items-center justify-center">
-      <h1 className="text-7xl text-white">Welcome to E-commerce</h1>
-    </main>
+    <Provider store={store}>
+      <main className="bg-slate-900 h-screen flex items-center justify-center">
+        <h1 className="text-7xl text-white">Welcome to E-commerce</h1>
+      </main>
+    </Provider>
   );
 }
 
