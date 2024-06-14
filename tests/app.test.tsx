cf58b5e3f6ecaf05d0d7670d70a79473fd38f5a4
shebz2023 <shebelleke@gmail.com>
2024-06-14 11:38:22 +0200

    chore-configure-eslint-precommithook ,this commit adds prettier too 187364801

--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest'
-import App from "../src/App";
+import { describe, expect, it } from 'vitest';
+import App from '../src/App';
 import React from 'react';
 
 describe('App', () => {
   it('renders headline', () => {
     render(<App />);
-    const headline = screen.getByText("Welcome to E-commerce");
+    const headline = screen.getByText('Welcome to E-commerce');
     expect(headline).toBeInTheDocument();
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +9,14 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['tests/setup.ts'],
     globals: true,
-    include: ['tests/**/*.test.{js,ts,jsx,tsx}', 'tests/**/*.spec.{js,ts,jsx,tsx}'],
+    include: [
+      'tests/**/*.test.{js,ts,jsx,tsx}',
+      'tests/**/*.spec.{js,ts,jsx,tsx}',
+    ],
     coverage: {
       include: ['src/**/*.{js,ts,jsx,tsx}'],
       provider: 'v8',
-      reporter: ['text', 'lcov']
+      reporter: ['text', 'lcov'],
     },
   },
-})
+});
