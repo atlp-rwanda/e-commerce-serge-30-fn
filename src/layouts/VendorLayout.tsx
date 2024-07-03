99bb7ebdf24c5d3f3582a0af38b1cee4eb7fa1dd
Aldo Twizerimana <aldotwizerimana@Aldos-MacBook-Pro.local>
2024-07-03 10:54:58 +0200

    fx(google-auth): getting authenticated user by extracting the token and encoded user from url parameters

--- src/layouts/VendorLayout.tsx
+++ src/layouts/VendorLayout.tsx
@@ -1,6 +1,31 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useLogoutMutation } from '../service/authApi';
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { setToken, setUser } from '../redux/features/auth/authSlice';
+import { useDispatch } from 'react-redux';
 export const VendorLayout: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const token = searchParams.get('token') || null;
+  const user = searchParams.get('user') || null;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (token != null) {
+      localStorage.setItem('token', token);
+      dispatch(setToken(token));
+    }
+
+    if (user != null) {
+      const decodedUser = decodeURIComponent(user);
+      const userObject = JSON.parse(decodedUser);
+
+      dispatch(setUser(userObject));
+    }
+    setSearchParams({});
+    window.history.replaceState({}, '', window.location.pathname);
+  }, [token, user, dispatch]);
   const [logout, { isLoading }] = useLogoutMutation();
   const navigate = useNavigate();
 
